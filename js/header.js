a7a3cf8b8cb22dd7a63fefbf1c0ae2b602de76a3
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-01-28 23:52:17 -0300

    Añadiendo funcionalidad a los botones imagen y texto, cada uno muestra la pestaña que corresponde

--- js/header.js
+++ js/header.js
@@ -2,6 +2,11 @@ const nav = document.getElementById('nav');
 const lightMode = document.getElementById('light-mode');
 const darkMode = document.getElementById('dark-mode');
 const background = document.getElementById('main');
+const imageButton = document.getElementById('image-button');
+const textButton = document.getElementById('text-button');
+
+const imageSection = document.getElementById('image-section');
+const textSection = document.getElementById('text-section');
 
 lightMode.addEventListener('click', () => {    
         document.body.classList.add('light-theme')        
@@ -17,4 +22,15 @@ darkMode.addEventListener('click', () => {
     background.classList.remove('light-background')
     lightMode.classList.remove('display-none')
     darkMode.classList.remove('display-block')
+});
+
+textButton.addEventListener('click', () => {
+    imageSection.style.display = 'none'
+    textSection.style.display = 'block'
+});
+
+
+imageButton.addEventListener('click', () => {
+    imageSection.style.display = 'block'
+    textSection.style.display = 'none'
 });
