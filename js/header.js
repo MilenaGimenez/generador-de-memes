041b2871fc6fe025f1e2b29d806613a5b588a6b1
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-02-04 18:36:42 -0300

    Añadiendo boton que cierra las pestañas de secciones imagen y texto

--- js/header.js
+++ js/header.js
@@ -4,7 +4,8 @@ const darkMode = document.getElementById('dark-mode');
 const background = document.getElementById('main');
 const imageButton = document.getElementById('image-button');
 const textButton = document.getElementById('text-button');
-const closeButton = document.getElementById('close-button');
+const closeButtonImage = document.getElementById('close-button-image');
+const closeButtonText = document.getElementById('close-button-text');
 const downloadMeme = document.getElementById('descargar');
 const memeSection = document.getElementById('meme-section')
 
@@ -12,7 +13,7 @@ const imageSection = document.getElementById('image-section');
 const textSection = document.getElementById('text-section');
 
 
-textSection.classList.add('display-none');
+/* textSection.classList.add('display-none'); */
 
 lightMode.addEventListener('click', () => {    
         document.body.classList.add('light-theme')        
@@ -31,22 +32,33 @@ darkMode.addEventListener('click', () => {
 });
 
 textButton.addEventListener('click', () => {
+    /* imageSection.style.right = '-25%'
+    textSection.style.right = '0px' */
     imageSection.style.display = 'none'
     textSection.style.display = 'block'
+    textSection.style.right = '0px'    
 });
-
 
 imageButton.addEventListener('click', () => {
+    /* imageSection.style.right = '0px'
+    textSection.style.right = '-25%' */
     imageSection.style.display = 'block'
     textSection.style.display = 'none'
+    /* textSection.style.right = '-25%' */
 });
 
-closeButton.addEventListener('click', () => {
-    textSection.classList.remove('display-none')   
-    textSection.style.display= 'none';
+// Boton cerrar seccion 
+
+closeButtonImage.addEventListener('click', () => {  
     imageSection.style.display= 'none';
 });
 
+closeButtonText.addEventListener('click', () => {     
+    textSection.style.display= 'none';
+});
+
+
+//boton descarga
 
 downloadMeme.addEventListener('click', (e) => {    
     domtoimage.toBlob(memeSection)
@@ -55,3 +67,15 @@ downloadMeme.addEventListener('click', (e) => {
     });
     
 });
+
+// responsive de secciones
+if(window.innerWidth > 1300){
+    window.addEventListener('resize', ()=>{
+        if(window.innerWidth > 1300){
+            imageSection.style.display = 'block';
+        } else {
+            imageSection.style.display ='none';
+            /* textSection.style.display = 'none'; */
+        };
+    });
+};
